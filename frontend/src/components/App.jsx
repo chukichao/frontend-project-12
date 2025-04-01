9d79360f069e39476095168d9f39ea3feeb8aeef
Maxim Chukichev <maxichukichev@gmail.com>
2025-04-01 22:48:21 +0300

    fix deploy

--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -1,61 +1,11 @@
-import { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
-
-import io from 'socket.io-client';
-
-import {
-  getToken,
-  getDefaultChannelId,
-  getCurrentChannelId,
-} from '../store/selectors';
-import { uiActions } from '../store/actions';
-import { getChannels, getMessages } from '../store/asyncActions';
-
 import '../assets/application.scss';
-
 import AppRouter from './AppRouter.jsx';
 
-const App = () => {
-  const dispatch = useDispatch();
-
-  const token = useSelector(getToken);
-
-  const currentChannelId = useSelector(getCurrentChannelId);
-  const defaultChannelId = useSelector(getDefaultChannelId);
-
-  useEffect(() => {
-    const socket = io();
-
-    socket.on('newMessage', () => {
-      dispatch(getMessages(token));
-    });
-
-    socket.on('newChannel', () => {
-      dispatch(getChannels(token));
-    });
-
-    socket.on('renameChannel', () => {
-      dispatch(getChannels(token));
-    });
-
-    socket.on('removeChannel', ({ id }) => {
-      if (currentChannelId === id) {
-        dispatch(uiActions.setCurrentChannel({ id: defaultChannelId }));
-      }
-
-      dispatch(getChannels(token));
-      dispatch(getMessages(token));
-    });
-
-    /* eslint-disable-next-line react-hooks/exhaustive-deps */
-  }, []);
-
-  return (
-    <BrowserRouter>
-      <AppRouter />
-    </BrowserRouter>
-  );
-};
+const App = () => (
+  <BrowserRouter>
+    <AppRouter />
+  </BrowserRouter>
+);
 
 export default App;
--- frontend/src/components/Chat.jsx
+++ frontend/src/components/Chat.jsx
@@ -1,7 +1,14 @@
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 
-import { getToken } from '../store/selectors';
+import io from 'socket.io-client';
+
+import {
+  getToken,
+  getDefaultChannelId,
+  getCurrentChannelId,
+} from '../store/selectors';
+import { uiActions } from '../store/actions';
 import { getChannels, getMessages } from '../store/asyncActions';
 
 import ChannelsList from './СhannelsList.jsx';
@@ -12,7 +19,33 @@ const Chat = () => {
 
   const token = useSelector(getToken);
 
+  const currentChannelId = useSelector(getCurrentChannelId);
+  const defaultChannelId = useSelector(getDefaultChannelId);
+
   useEffect(() => {
+    const socket = io();
+
+    socket.on('newMessage', () => {
+      dispatch(getMessages(token));
+    });
+
+    socket.on('newChannel', () => {
+      dispatch(getChannels(token));
+    });
+
+    socket.on('renameChannel', () => {
+      dispatch(getChannels(token));
+    });
+
+    socket.on('removeChannel', ({ id }) => {
+      if (currentChannelId === id) {
+        dispatch(uiActions.setCurrentChannel({ id: defaultChannelId }));
+      }
+
+      dispatch(getChannels(token));
+      dispatch(getMessages(token));
+    });
+
     dispatch(getChannels(token));
     dispatch(getMessages(token));
 
